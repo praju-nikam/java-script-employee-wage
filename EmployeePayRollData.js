04ac2b8e79e246ea5c69e59b9fb18cafd7197a82
praju-nikam <sawanprajakta84@gmail.com>
2022-04-26 15:30:49 +0530

    Add:ability to create employee payroll data with id,name,salary

---
+++ EmployeePayRollData.js
@@ -0,0 +1,44 @@
+class EmployeePayRollUc12
+// property
+{
+id;
+salary;
+gender;
+startDate;
+
+//constructor
+
+constructor(...parans)
+{
+        this.id = parans[0];
+        this.name=parans[1];
+        this.salary=parans[2];
+        this.gender=parans[3];
+        this.startDate=parans[4];
+}
+
+//getter and setter method
+get name()
+{
+return this._name;
+}
+set name(name)
+{
+this._name=name;
+}
+
+//method
+toString()
+{
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const empDate=this.startDate ===undefined ? "undefined":
+                                          this.startDate.toLocaleDateString("en-US",options);
+        return "id = " +this.id + ", name = "+this.name+", salary = "+this.salary +","+
+                                " gender = "+this.gender+", startDate = " +empDate;
+}
+}
+let newEmployeePayRollUc12=new EmployeePayRollUc12(1,"Ankita",30000,"F",new Date());
+console.log(newEmployeePayRollUc12.toString());
+let employeePayRollUc12 = new EmployeePayRollUc12(2,"Sanket",33000,"M",new Date());
+console.log(employeePayRollUc12.toString());
+
